231a14c2c04f35d73aa62c832818a225fb8a7500
leone-nyaga <leonenyaga@gmail.com>
2024-11-28 12:21:59 +0000

    GREEDY ALGORITHM

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,23 @@
-#!/bin/python3
+#!/usr/bin/python3
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
+
+
 def makeChange(coins, total):
-    """
-    Determines the fewest number of coins needed to make the given total.
-
-    Args:
-        coins (list): List of available coin denominations.
-        total (int): The total amount to make change for.
-
-    Returns:
-        int: The fewest number of coins needed to meet the total,
-             or -1 if it's not possible to make the total with the given coins.
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
     """
     if total <= 0:
         return 0
-    
-    dp = [float('inf')] * (total + 1)
-    
-    dp[0] = 0
-    
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
 
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
